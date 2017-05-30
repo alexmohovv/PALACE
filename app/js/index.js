c06721035f3a054daa4b3aba22f1973894032e34
Alex <alexmohovvv@gmail.com>
2017-05-30 21:02:18 +0600

    add progress bar add new styles, delete jquery funcs

--- app/js/index.js
+++ app/js/index.js
@@ -1,10 +1,2 @@
 var $ = require('jQuery');
-/*
-$('.file').click(function(){
-    if($(this).hasClass('active')){
-        $(this).removeClass('active');
-    } else {
-        $(this).addClass('active')
-    }
-});
-*/
+
