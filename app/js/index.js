59042e5c27359f736b821e8131185a2a8f8b87fe
Alex <alexmohovvv@gmail.com>
2017-05-31 15:02:22 +0600

    refactor jquery, fix bags

--- app/js/index.js
+++ app/js/index.js
@@ -2,15 +2,16 @@ var $ = require('jQuery');
 
 $(function(){
     $("button").click(function(){
-        $("div:visible:not(.headPanel)").hide();
 
         if ($(this).is("button.file")) {
+            $("div.menu").hide();
             $("button").removeClass("active");
             $(this).addClass("active");
             $("div.menuFiles").show();
         }
 
         if ($(this).is("button.chains")) {
+            $("div.menu").hide();
             $("button").removeClass("active");
             $(this).addClass("active");
             $("div.menuChains").show();
