6b09e0a57ed2140f20e36c241e402ba2b60bd648
Alex <alexmohovvv@gmail.com>
2017-05-31 09:23:15 +0600

    add awesome jquery, some correct css and html

--- app/js/index.js
+++ app/js/index.js
@@ -1,2 +1,21 @@
 var $ = require('jQuery');
 
+$(function(){
+    $("button").click(function(){
+        $("div:visible:not(.headPanel)").hide();
+
+        if ($(this).is("button.file")) {
+            $("button").removeClass("active");
+            $(this).addClass("active");
+            $("div.menuFiles").show();
+        }
+
+        if ($(this).is("button.chains")) {
+            $("button").removeClass("active");
+            $(this).addClass("active");
+            $("div.menuChains").show();
+        }
+    })
+})
+
+
